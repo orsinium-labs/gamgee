5e8d2aeabdff4c44f846375af8bba7c9184fccf9
gram <git@orsinium.dev>
2024-02-05 18:29:18 +0100

    try wazero

--- bridge.go
+++ bridge.go
@@ -1,17 +1,5 @@
 package main
 
-import "github.com/perlin-network/life/exec"
-
 type Bridge struct {
 	Device *Device
-}
-
-func (b *Bridge) ResolveFunc(module, field string) exec.FunctionImport {
-	// exec.FunctionImport{}
-	return nil
-}
-
-func (b *Bridge) ResolveGlobal(module, field string) int64 {
-	//...
-	return 0
 }
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"context"
 	_ "embed"
-	"errors"
-	"fmt"
 
-	"github.com/perlin-network/life/exec"
+	"github.com/tetratelabs/wazero"
 )
 
 //go:embed demo.wat
@@ -14,21 +13,9 @@ var binaryModule []byte
 func run() error {
 	device := NewDevice()
 	_ = device
-	// Leds(&device)
-	vm, err := exec.NewVirtualMachine(binaryModule, exec.VMConfig{}, &exec.NopResolver{}, nil)
-	if err != nil { // if the wasm bytecode is invalid
-		return err
-	}
-	entryID, ok := vm.GetFunctionExport("update")
-	if !ok {
-		return errors.New("cannot find function `update`")
-	}
-	ret, err := vm.Run(entryID)
-	if err != nil {
-		vm.PrintStackTrace()
-		return err
-	}
-	fmt.Printf("return value = %d\n", ret)
+	ctx := context.Background()
+	r := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfigInterpreter())
+	defer r.Close(ctx)
 	return nil
 }
 
