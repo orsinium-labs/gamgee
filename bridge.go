82945a23d5287e55f2679dce3012abdd1e1bc6f5
gram <git@orsinium.dev>
2024-02-07 13:06:46 +0100

    wrap printed text

--- bridge.go
+++ bridge.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"strconv"
+	"strings"
 
 	"tinygo.org/x/tinyfont"
 	"tinygo.org/x/tinyfont/freesans"
@@ -22,15 +23,27 @@ func (b *Bridge) EchoI32(x int32) {
 }
 
 func (b *Bridge) EchoText(text string) {
+	text = hardWrap(text, 18)
 	b.d.Display.FillScreen(white)
 	font := freesans.Regular9pt7b
-	w32, _ := tinyfont.LineWidth(&font, text)
+	// w32, _ := tinyfont.LineWidth(&font, text)
 	tinyfont.WriteLineColors(
 		b.d.Display,
 		&font,
-		(width-int16(w32))/2,
-		72,
+		1,
+		10,
 		text,
 		[]color.RGBA{red},
 	)
+}
+
+func hardWrap(text string, colBreak int) string {
+	text = strings.TrimSpace(text)
+	wrapped := ""
+	var i int
+	for i = 0; len(text[i:]) > colBreak; i += colBreak {
+		wrapped += text[i:i+colBreak] + "\n"
+	}
+	wrapped += text[i:]
+	return wrapped
 }
