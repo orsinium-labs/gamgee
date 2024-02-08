a9953f701367bcf5a358d53337e4d3b87384f4d9
gram <git@orsinium.dev>
2024-02-08 11:55:16 +0100

    write text

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 #![no_std]
 #![no_main]
 
-use pybadge_high::{prelude::*, PyBadge};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{PrimitiveStyleBuilder, Rectangle},
+    text::Text,
+};
+use pybadge::{prelude::*, *};
+use pybadge_high as pybadge;
+
+const NUM_LEDS: u8 = 5;
 
 #[entry]
 fn main() -> ! {
     let mut pybadge = PyBadge::take().unwrap();
+    pybadge.display.clear(Color::RED).unwrap();
+
+    let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+    Text::new("Hello Rust!", Point::new(20, 30), style)
+        .draw(&mut pybadge.display)
+        .unwrap();
+
     loop {}
 }
