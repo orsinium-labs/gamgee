27b9246021424b04de069426f9a60ea78d50240b
gram <git@orsinium.dev>
2024-02-11 16:53:07 +0100

    pass data into every function

--- src/bridge.rs
+++ src/bridge.rs
@@ -29,11 +29,13 @@ impl Bridge {
     /// Initialize memory and stuff. Called before the application is started.
     pub fn init(&mut self, memory: wasmi::Memory, data: &mut [u8]) {
         self.memory = Some(memory);
-        // init the palette
+        // init the color palette
         write32le(&mut data[PALETTE..], 0xe0f8cf); // light
         write32le(&mut data[PALETTE + 4..], 0x86c06c);
         write32le(&mut data[PALETTE + 8..], 0x306850);
         write32le(&mut data[PALETTE + 12..], 0x071821); // dark
+        write16le(&mut data[DRAW_COLORS..], 0x0312);
+        write32le(&mut data[MOUSE_X..], 0x7fff_7fff);
 
         // https://lospec.com/palette-list/ice-cream-gb
         // write32le(&mut data[PALETTE..], 0xfff6d3); // light
@@ -100,63 +102,70 @@ impl Bridge {
         // ...
     }
 
-    pub fn wasm4_line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32) {
-        // ...
+    pub fn wasm4_hline(&mut self, data: &mut [u8], x: i32, y: i32, len: i32) {
+        self.wasm4_line(data, x, y, x + len, y)
     }
 
-    pub fn wasm4_hline(&mut self, data: &mut [u8], x: i32, y: i32, len: i32) {
-        let line = Line::new(Point { x, y }, Point { x: x + len, y });
+    pub fn wasm4_vline(&mut self, data: &mut [u8], x: i32, y: i32, len: i32) {
+        self.wasm4_line(data, x, y, x, y + len)
+    }
+
+    pub fn wasm4_line(&mut self, data: &mut [u8], x1: i32, y1: i32, x2: i32, y2: i32) {
+        let line = Line::new(Point::new(x1, y1), Point::new(x2, y2));
         let mut frame_buf = FrameBuf::from_memory(data);
         let color = Color4(2);
         let style = PrimitiveStyle::with_stroke(color, 1);
         line.draw_styled(&style, &mut frame_buf).unwrap();
     }
 
-    pub fn wasm4_vline(&mut self, x: i32, y: i32, len: u32) {
+    pub fn wasm4_oval(&mut self, data: &mut [u8], x: i32, y: i32, width: u32, height: u32) {
         // ...
     }
 
-    pub fn wasm4_oval(&mut self, x: i32, y: i32, width: u32, height: u32) {
+    pub fn wasm4_rect(&mut self, data: &mut [u8], x: i32, y: i32, width: u32, height: u32) {
         // ...
     }
 
-    pub fn wasm4_rect(&mut self, x: i32, y: i32, width: u32, height: u32) {
+    pub fn wasm4_text(&mut self, data: &mut [u8], text: i32, x: i32, y: i32) {
         // ...
     }
 
-    pub fn wasm4_text(&mut self, text: i32, x: i32, y: i32) {
+    pub fn wasm4_text_utf8(&mut self, data: &mut [u8], text: i32, byte_len: u32, x: i32, y: i32) {
         // ...
     }
 
-    pub fn wasm4_text_utf8(&mut self, text: i32, byte_len: u32, x: i32, y: i32) {
+    pub fn wasm4_text_utf16(&mut self, data: &mut [u8], text: i32, byte_len: u32, x: i32, y: i32) {
         // ...
     }
 
-    pub fn wasm4_text_utf16(&mut self, text: i32, byte_len: u32, x: i32, y: i32) {
+    pub fn wasm4_tone(
+        &mut self,
+        data: &mut [u8],
+        frequency: u32,
+        duration: u32,
+        volume: u32,
+        flags: u32,
+    ) {
         // ...
     }
 
-    pub fn wasm4_tone(&mut self, frequency: u32, duration: u32, volume: u32, flags: u32) {
-        // ...
-    }
-
-    pub fn wasm4_diskr(&mut self, dest: i32, size: u32) -> i32 {
+    pub fn wasm4_diskr(&mut self, data: &mut [u8], dest: i32, size: u32) -> i32 {
         0
     }
 
-    pub fn wasm4_diskw(&mut self, src: i32, size: u32) -> i32 {
+    pub fn wasm4_diskw(&mut self, data: &mut [u8], src: i32, size: u32) -> i32 {
         0
     }
 
-    pub fn wasm4_trace(&mut self, str: i32) {
+    pub fn wasm4_trace(&mut self, data: &mut [u8], str: i32) {
         // ...
     }
 
-    pub fn wasm4_trace_utf8(&mut self, str: i32, byte_len: u32) {
+    pub fn wasm4_trace_utf8(&mut self, data: &mut [u8], str: i32, byte_len: u32) {
         // ...
     }
 
-    pub fn wasm4_trace_utf16(&mut self, str: i32, byte_len: u32) {
+    pub fn wasm4_trace_utf16(&mut self, data: &mut [u8], str: i32, byte_len: u32) {
         // ...
     }
 }
@@ -167,4 +176,10 @@ fn write32le(target: &mut [u8], val: u32) {
     target[2] = ((val & 0x0000_ff00) >> 8) as u8;
     target[1] = ((val & 0x00ff_0000) >> 16) as u8;
     target[0] = ((val & 0xff00_0000) >> 24) as u8;
+}
+
+fn write16le(target: &mut [u8], val: u16) {
+    let val = val.to_le();
+    target[1] = (val & 0x00ff) as u8;
+    target[0] = ((val & 0xff00) >> 8) as u8;
 }
--- src/linking.rs
+++ src/linking.rs
@@ -7,13 +7,16 @@ pub fn link(
     linker: &mut wasmi::Linker<Bridge>,
     memory: wasmi::Memory,
 ) -> Result<(), wasmi::errors::LinkerError> {
-    linker.func_wrap("pybadge", "echo_i32", |mut caller: C, param: i32| {
-        caller.data_mut().echo_i32(param)
-    })?;
     linker.func_wrap(
         "env",
         "blit",
-        |mut caller: C, sprite_ptr: i32, x: i32, y: i32, width: u32, height: u32, flags: u32| {
+        move |mut caller: C,
+              sprite_ptr: i32,
+              x: i32,
+              y: i32,
+              width: u32,
+              height: u32,
+              flags: u32| {
             caller
                 .data_mut()
                 .wasm4_blit(sprite_ptr, x, y, width, height, flags);
@@ -22,16 +25,16 @@ pub fn link(
     linker.func_wrap(
         "env",
         "blitSub",
-        |mut caller: C,
-         sprite_ptr: i32,
-         x: i32,
-         y: i32,
-         width: u32,
-         height: u32,
-         src_x: u32,
-         src_y: u32,
-         stride: i32,
-         flags: u32| {
+        move |mut caller: C,
+              sprite_ptr: i32,
+              x: i32,
+              y: i32,
+              width: u32,
+              height: u32,
+              src_x: u32,
+              src_y: u32,
+              stride: i32,
+              flags: u32| {
             caller
                 .data_mut()
                 .wasm4_blit_sub(sprite_ptr, x, y, width, height, src_x, src_y, stride, flags)
@@ -40,8 +43,9 @@ pub fn link(
     linker.func_wrap(
         "env",
         "line",
-        |mut caller: C, x1: i32, y1: i32, x2: i32, y2: i32| {
-            caller.data_mut().wasm4_line(x1, y1, x2, y2)
+        move |mut caller: C, x1: i32, y1: i32, x2: i32, y2: i32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_line(data, x1, y1, x2, y2)
         },
     )?;
     linker.func_wrap(
@@ -52,67 +56,93 @@ pub fn link(
             bridge.wasm4_hline(data, x, y, len)
         },
     )?;
-    linker.func_wrap("env", "vline", |mut caller: C, x: i32, y: i32, len: u32| {
-        caller.data_mut().wasm4_vline(x, y, len)
-    })?;
+    linker.func_wrap(
+        "env",
+        "vline",
+        move |mut caller: C, x: i32, y: i32, len: i32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_vline(data, x, y, len)
+        },
+    )?;
     linker.func_wrap(
         "env",
         "oval",
-        |mut caller: C, x: i32, y: i32, width: u32, height: u32| {
-            caller.data_mut().wasm4_oval(x, y, width, height)
+        move |mut caller: C, x: i32, y: i32, width: u32, height: u32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_oval(data, x, y, width, height)
         },
     )?;
     linker.func_wrap(
         "env",
         "rect",
-        |mut caller: C, x: i32, y: i32, width: u32, height: u32| {
-            caller.data_mut().wasm4_rect(x, y, width, height)
+        move |mut caller: C, x: i32, y: i32, width: u32, height: u32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_rect(data, x, y, width, height)
         },
     )?;
-    linker.func_wrap("env", "text", |mut caller: C, text: i32, x: i32, y: i32| {
-        caller.data_mut().wasm4_text(text, x, y)
-    })?;
+    linker.func_wrap(
+        "env",
+        "text",
+        move |mut caller: C, text: i32, x: i32, y: i32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_text(data, text, x, y)
+        },
+    )?;
     linker.func_wrap(
         "env",
         "textUtf8",
-        |mut caller: C, text: i32, byte_len: u32, x: i32, y: i32| {
-            caller.data_mut().wasm4_text_utf8(text, byte_len, x, y)
+        move |mut caller: C, text: i32, byte_len: u32, x: i32, y: i32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_text_utf8(data, text, byte_len, x, y)
         },
     )?;
     linker.func_wrap(
         "env",
         "textUtf16",
-        |mut caller: C, text: i32, byte_len: u32, x: i32, y: i32| {
-            caller.data_mut().wasm4_text_utf16(text, byte_len, x, y)
+        move |mut caller: C, text: i32, byte_len: u32, x: i32, y: i32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_text_utf16(data, text, byte_len, x, y)
         },
     )?;
     linker.func_wrap(
         "env",
         "tone",
-        |mut caller: C, frequency: u32, duration: u32, volume: u32, flags: u32| {
-            caller
-                .data_mut()
-                .wasm4_tone(frequency, duration, volume, flags)
+        move |mut caller: C, frequency: u32, duration: u32, volume: u32, flags: u32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_tone(data, frequency, duration, volume, flags)
         },
     )?;
-    linker.func_wrap("env", "diskr", |mut caller: C, dest: i32, size: u32| {
-        caller.data_mut().wasm4_diskr(dest, size)
+    linker.func_wrap(
+        "env",
+        "diskr",
+        move |mut caller: C, dest: i32, size: u32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_diskr(data, dest, size)
+        },
+    )?;
+    linker.func_wrap("env", "diskw", move |mut caller: C, src: i32, size: u32| {
+        let (data, bridge) = memory.data_and_store_mut(&mut caller);
+        bridge.wasm4_diskw(data, src, size)
     })?;
-    linker.func_wrap("env", "diskw", |mut caller: C, src: i32, size: u32| {
-        caller.data_mut().wasm4_diskw(src, size)
-    })?;
-    linker.func_wrap("env", "trace", |mut caller: C, str: i32| {
-        caller.data_mut().wasm4_trace(str)
+    linker.func_wrap("env", "trace", move |mut caller: C, str: i32| {
+        let (data, bridge) = memory.data_and_store_mut(&mut caller);
+        bridge.wasm4_trace(data, str)
     })?;
     linker.func_wrap(
         "env",
         "traceUtf8",
-        |mut caller: C, str: i32, byte_len: u32| caller.data_mut().wasm4_trace_utf8(str, byte_len),
+        move |mut caller: C, str: i32, byte_len: u32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_trace_utf8(data, str, byte_len)
+        },
     )?;
     linker.func_wrap(
         "env",
         "traceUtf16",
-        |mut caller: C, str: i32, byte_len: u32| caller.data_mut().wasm4_trace_utf16(str, byte_len),
+        move |mut caller: C, str: i32, byte_len: u32| {
+            let (data, bridge) = memory.data_and_store_mut(&mut caller);
+            bridge.wasm4_trace_utf16(data, str, byte_len)
+        },
     )?;
 
     Ok(())
