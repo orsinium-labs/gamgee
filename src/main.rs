1ef6bd4969b8d0127270e813441b5d63e93a49f7
gram <git@orsinium.dev>
2024-02-13 21:29:22 +0100

    use all memory for the heap

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ static HEAP: Heap = Heap::empty();
 fn main() -> ! {
     {
         use core::mem::MaybeUninit;
-        const HEAP_SIZE: usize = 128 * 1024;
+        const HEAP_SIZE: usize = 168 * 1024;
         static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
         unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
     }
