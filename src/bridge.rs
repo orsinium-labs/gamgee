be460355aa0acf97c9b583da645dd979638a8d58
gram <git@orsinium.dev>
2024-02-20 11:03:49 +0100

    fix blit_sub slicing

--- src/bridge.rs
+++ src/bridge.rs
@@ -147,7 +147,7 @@ impl Bridge {
         flags: u32,
     ) {
         let memory = Memory::from_bytes(data);
-        let size = (stride * height) as u32 / 4;
+        let size = (stride * (src_y + height)) as u32 / 4;
         let sprite = get_user_data(memory.user_data, sprite_ptr, size);
         let mut frame_buf = FrameBuf {
             palette_raw: memory.palette,
